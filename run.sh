a4bcbe10fe8ca50761046980ee72873d24153986
Egor I <egor.ivanov@pm.me>
2025-11-02 10:38:17 +0100

    add sh
    Signed-off-by: Egor I <egor.ivanov@pm.me>

---
+++ run.sh
@@ -0,0 +1,10 @@
+pnpm allure run --config=./allurerc.mjs -- pnpm test
+
+if [ $? -ne 0 ]; then
+    echo "Tests failed. Exiting..."
+    exit 1
+fi
+
+pnpm allure generate --config=./allurerc.mjs
+
+pnpm allure open --config=./allurerc.mjs
