0a9d67ec0c947cc5509509a3d00ca983059cacdb
Egor I <egor.ivanov@pm.me>
2025-12-10 14:29:57 +0100

    less chance of failing
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/utils.js
+++ test/utils.js
@@ -22,15 +22,15 @@ const attachMicroservice = async (val) => {
 const isTimeToThrow = () => {
   var failing = Math.random();
 
-  if (failing > 0.91 && failing < 0.92) {
+  if (failing > 0.92 && failing < 0.92) {
     throw new Error("net::ERR_CONNECTION_REFUSED");
   }
-  if (failing > 0.94 && failing < 0.95) {
+  if (failing > 0.95 && failing < 0.95) {
     throw new Error(
       "Element not found {selector: something}\n Expected: visible or transparent: visible or have css value opacity=0\n Timeout: 6000 ms"
     );
   }
-  if (failing > 0.97 && failing < 0.98) {
+  if (failing > 0.98 && failing < 0.98) {
     throw new Error("Test timeout of 30000ms exceeded.");
   }
 };
